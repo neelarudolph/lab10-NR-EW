302536d70b5bedb8d6a01fc6070ed85ab40fbf66
neelarudolph <neela.rudolph@ufl.edu>
2025-11-16 10:00:39 -0500

    calculator.py

--- calculator.py
+++ calculator.py
@@ -1,11 +1,21 @@
-"""
-calculator.py
-- Defines functions used to create a simple calculator
+import math
 
-One function per operation, in order.
-"""
-# First example
-def add(a, b): 
-    pass
+def add(a, b):
+    return a + b
 
+def subtract(a, b):
+    return a - b
 
+def multiply(a, b):
+    return a * b
+
+def divide(a, b):
+    if a == 0:
+        raise ZeroDivisionError("Division by zero is not allowed.")
+    return b / a    # raise ZeroDivisionError if a == 0
+
+def logarithm(a, b):
+    return math.log(b,a)
+
+def exponent(a, b):
+    return a ** b
