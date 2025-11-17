8de8d9512f3c3281ff5479b52742a94e919af7de
edub966 <erikwoods17@gmail.com>
2025-11-16 21:29:02 -0500

    Update test_calculator.py
    will be changing soon bc i think there are indentation and function name errors

--- test_calculator.py
+++ test_calculator.py
@@ -9,11 +9,17 @@ class TestCalculator(unittest.TestCase):
     ######### Partner 2
     # def test_add(self): # 3 assertions
     #     fill in code
-
+    def test_add(self):  # 3 assertions
+        self.assertEqual(add(2, 3), 5)
+        self.assertEqual(add(-1, 1), 0)
+        self.assertEqual(add(0, 0), 0)
     # def test_subtract(self): # 3 assertions
     #     fill in code
     # ##########################
-
+ def test_subtract(self):  # 3 assertions
+        self.assertEqual(sub(5, 3), 2)
+        self.assertEqual(sub(0, 5), -5)
+        self.assertEqual(sub(-2, -3), 1)
     ######## Partner 1
     # def test_multiply(self): # 3 assertions
     #     fill in code
@@ -36,15 +42,23 @@ def test_divide(self):
     #     # with self.assertRaises(<INSERT_ERROR_TYPE>):
     #     #     div(0, 5)
     #     fill in code
-
+ def test_divide_by_zero(self):  # 1 assertion
+        with self.assertRaises(ZeroDivisionError):
+            div(0, 5)
     # def test_logarithm(self): # 3 assertions
     #     fill in code
+ def test_logarithm(self):  # 3 assertions
+        self.assertAlmostEqual(log(10, 100), 2.0, places=7)
+        self.assertAlmostEqual(log(2, 8), 3.0, places=7)
+        self.assertAlmostEqual(log(3, 9), 2.0, places=7)
 
     # def test_log_invalid_base(self): # 1 assertion
     #     # use same technique from test_divide_by_zero
     #     fill in code
     # ##########################
-    
+    def test_log_invalid_base(self):  # 1 assertion
+        with self.assertRaises(ValueError):
+            log(1, 5)
     ######## Partner 1
     # def test_log_invalid_argument(self): # 1 assertion
     #     # call log function inside, example:
