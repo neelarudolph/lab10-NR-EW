926424e05f7cfe7e9d984661528e85b9249006bd
edub966 <erikwoods17@gmail.com>
2025-11-16 21:33:55 -0500

    Update calculator.py
    wrong names lmao

--- calculator.py
+++ calculator.py
@@ -17,19 +17,19 @@ def hypotenuse(a, b):
 def add(a, b):
     return a + b
 
-def subtract(a, b):
+def sub(a, b):
     return a - b
 
-def multiply(a, b):
+def mul(a, b):
     return a * b
 
-def divide(a, b):
+def div(a, b):
     if a == 0:
         raise ZeroDivisionError("Division by zero is not allowed.")
     return b / a    # raise ZeroDivisionError if a == 0
 
-def logarithm(a, b):
+def log(a, b):
     return math.log(b,a)
 
-def exponent(a, b):
+def exp(a, b):
     return a ** b
