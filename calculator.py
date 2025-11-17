6f0f99c275092ac690d1a238b96b114c7c6787ff
edub966 <erikwoods17@gmail.com>
2025-11-16 21:29:47 -0500

    Update calculator.py

--- calculator.py
+++ calculator.py
@@ -4,6 +4,28 @@
 
 import math
 
+def add(a, b):
+    return a + b
+
+def sub(a, b):
+    return a - b
+
+def mul(a, b):
+    return a * b
+
+def div(a, b):
+    if a == 0:
+        raise ZeroDivisionError("division by zero")
+    return b / a
+
+def log(a, b):
+    if a <= 0 or a == 1 or b <= 0:
+        raise ValueError("math domain error")
+    return math.log(b, a)
+
+def exp(a, b):
+    return a ** b
+
 def square_root(a):
     # raise ValueError if a < 0
     if a < 0:
